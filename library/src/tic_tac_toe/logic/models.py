cf8cbe2b51183aeb664c2307a7f9421d3a6fed4e
jsninoladino <60367340+jsninoladino@users.noreply.github.com>
2022-11-07 21:58:03 -0800

    creating tic-tac-toe library

---
+++ library/src/tic_tac_toe/logic/models.py
@@ -0,0 +1,12 @@
+# tic_tac_toe/logic/models.py
+
+import enum
+
+class Mark(str, enum.Enum):
+    CROSS = "X"
+    NAUGHT = "O"
+
+    @property
+    def other(self) -> "Mark":
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+        
